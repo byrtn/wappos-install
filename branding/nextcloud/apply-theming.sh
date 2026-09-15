3698265489e3ca6bbf9c68642b9401bb247bf75d
Patrick Ritaine <pritaine@gmail.com>
2026-09-15 18:04:03 +0200

    Ajoute le logo Wappos blanc pour Nextcloud

--- branding/nextcloud/apply-theming.sh
+++ branding/nextcloud/apply-theming.sh
@@ -6,9 +6,9 @@ BRANDING_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DATA_DIR="/home/yunohost.app/nextcloud/data"
 OCC="sudo -u nextcloud php /var/www/nextcloud/occ"
 
-cp "$BRANDING_DIR/wappos-logo.png" "$DATA_DIR/wappos-logo.png"
+cp "$BRANDING_DIR/wappos-logo-white.png" "$DATA_DIR/wappos-logo-white.png"
 cp "$BRANDING_DIR/wappos-favicon.png" "$DATA_DIR/wappos-favicon.png"
-chown nextcloud:nextcloud "$DATA_DIR/wappos-logo.png" "$DATA_DIR/wappos-favicon.png"
+chown nextcloud:nextcloud "$DATA_DIR/wappos-logo-white.png" "$DATA_DIR/wappos-favicon.png"
 
 $OCC theming:config name "Wappos - Cloud"
 $OCC theming:config slogan "Souveraineté numérique"
@@ -16,8 +16,8 @@ $OCC theming:config primary_color "#016f93"
 if [ -n "${1:-}" ]; then
     $OCC theming:config url "https://$1"
 fi
-$OCC theming:config logo "$DATA_DIR/wappos-logo.png"
-$OCC theming:config logoheader "$DATA_DIR/wappos-logo.png"
+$OCC theming:config logo "$DATA_DIR/wappos-logo-white.png"
+$OCC theming:config logoheader "$DATA_DIR/wappos-logo-white.png"
 $OCC theming:config favicon "$DATA_DIR/wappos-favicon.png"
 
-rm -f "$DATA_DIR/wappos-logo.png" "$DATA_DIR/wappos-favicon.png"
+rm -f "$DATA_DIR/wappos-logo-white.png" "$DATA_DIR/wappos-favicon.png"
